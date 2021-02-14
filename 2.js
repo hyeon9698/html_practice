57659efb2f320473c40d2267c194105a5be9abc9
DonghyeonCho <hyeon9698@naver.com>
2021-02-14 07:49:16 +0000

    html

--- 2.js
+++ 2.js
@@ -1,7 +1,4 @@
-var t = document.getElementById('target');
-t.addEventListener('submit', function(event){
-    if(document.getElementById('name').value.length === 0){
-        alert('Name 필드의 값이 누락 되었습니다');
-        event.preventDefault();
-    }
+var t = document.querySelector('#target');
+t.addEventListener('change', function(event){
+	document.querySelector('#result').innerHTML = event.target.value;
 });
